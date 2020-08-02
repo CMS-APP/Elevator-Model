373a53d5fb12873909aba78fe72b53aa3086f789
Chris-App <Christopher.sharp@hotmail.co.uk>
2020-08-02 11:13:52 +0100

    Complete model overhaul

--- elevator_sim/elevator_network.py
+++ elevator_sim/elevator_network.py
@@ -1,137 +1,125 @@
-from elevator_sim.objects import Passenger
 import random
+
+from elevator_sim.objects import Passenger
 
 
 class ElevatorNetwork:
-    def __init__(self, floors, elevator, passengers, time_limit, frame_rate, mode):
+    def __init__(self, floors, elevator, passengers, frame_rate, mode):
         super().__init__()
         self.floors = floors
         self.elevator = elevator
         self.passengers = passengers
-        self.time_limit = time_limit
         self.frame_rate = frame_rate
         self.mode = mode
-        self.modes = ['linear', 'first_come', 'point_system']
+        self.modes = ['linear', 'first_come']
         self.animation = False
         self.real_time = 0
         self.time = 0
         self.all_passengers = []
-        self.elevator.target_passenger = ""
-        self.elevator.target = 'ground'
-        self.elevator.second_target = ''
+        self.passenger_number = 0
+        self.elevator.passenger_target = None
+        self.elevator.target = None
+        self.elevator.second_target = None
 
-    def new_passengers(self, passenger_num):
+    def new_passengers(self):
         for floor in self.floors:
             if random.random() < floor.spawn_rate:
+                self.passenger_number += 1
                 possible_destinations = [x for x in self.floors if x != floor]
-                passenger_num += 1
-                passenger = Passenger("Passenger " + str(passenger_num), floor.name, random.choice(possible_destinations).name)
+                passenger = Passenger(name="Passenger " + str(self.passenger_number), source=floor,
+                                      destination=random.choice(possible_destinations))
                 floor.passengers.append(passenger)
                 self.all_passengers.append(passenger)
-                print("(" + str(self.real_time) + " wait), " + passenger.name + ", " + floor.name + ", " + passenger.destination)
-        return passenger_num
+                print(f"( {self.real_time} wait), {passenger.name}, {floor.name}, {passenger.destination.name}")
 
-    def idle_events(self, floor):
-        all_elevator_alight = True
-        all_elevator_board = True
-        leaving_passengers = []
+    def stop_check(self, floor):
+        if len(self.elevator.passengers) != 0:
+            for passenger in self.elevator.passengers:
+                if passenger.destination == floor:
+                    return True
 
-        for elevator_passenger in self.elevator.passengers:
-            if elevator_passenger.destination == floor.name:
-                leaving_passengers.append(elevator_passenger)
-        if len(leaving_passengers) >= 1:
-            all_elevator_alight = False
-            for leaving_passenger in leaving_passengers:
-                self.all_passengers.remove(leaving_passenger)
-                self.elevator.passengers.remove(leaving_passenger)
-                if leaving_passenger.name == self.elevator.target_passenger:
-                    self.elevator.target_passenger = ''
-                print("(" + str(self.real_time) + " alight), " + leaving_passenger.name + ", " + floor.name + ", " + leaving_passenger.destination)
-                break
+        if len(floor.passengers) != 0 and len(self.elevator.passengers) != self.elevator.capacity:
+            return True
 
-        if all_elevator_alight:
-            for floor_passenger in floor.passengers:
-                if len(self.elevator.passengers) < self.elevator.capacity:
-                    all_elevator_board = False
-                    self.elevator.passengers.append(floor_passenger)
-                    floor.passengers.remove(floor_passenger)
-                    print("(" + str(self.real_time) + " board), " + floor_passenger.name + ", " + floor.name + ", " + floor_passenger.destination)
-                    break
-                elif len(self.elevator.passengers) == self.elevator.capacity:
-                    all_elevator_board = True
+        return False
 
-        return all_elevator_alight, all_elevator_board
+    def floor_action(self, floor):
+        if len(self.elevator.passengers) != 0:
+            for passenger in self.elevator.passengers:
+                if passenger.destination == floor:
+                    self.elevator.passengers.remove(passenger)
+                    self.all_passengers.remove(passenger)
+                    if self.elevator.passenger_target == passenger:
+                        self.elevator.passenger_target = None
+                    print(f'({self.real_time} leave), {passenger.name}, {floor.name}')
+                    return
+
+        if len(floor.passengers) != 0:
+            passenger = floor.passengers[0]
+            self.elevator.passengers.append(passenger)
+            print(
+                f'({self.real_time} board), {passenger.name}, {floor.name}, {passenger.destination.name}')
+            floor.passengers.remove(floor.passengers[0])
+            return
 
     def linear_simulation(self):
-        """ A linear simulation of an elevator, moving up and down like a bus.
-
-        The main part of the algorithm includes
-        """
-        if self.elevator.mode != 'leaving':
-            for floor in self.floors:
-                if floor.y == self.elevator.y:
-                    if self.elevator.mode == 'moving':
-
-                        if floor.floor_type == 'end':
-                            self.elevator.direction *= -1
-                            self.elevator.speed, self.elevator.mode = 0, 'turning'
-
-                        for passenger in self.elevator.passengers:
-                            if passenger.destination == floor.name:
-                                self.elevator.speed, self.elevator.mode = 0, 'boarding'
-
-                        if len(floor.passengers) != 0:
-                            self.elevator.speed, self.elevator.mode = 0, 'boarding'
-
-                    elif self.elevator.mode == 'boarding':
-                        all_elevator_alight, all_elevator_board = self.idle_events(floor)
-                        if all_elevator_alight and all_elevator_board:
-                            self.elevator.speed, self.elevator.mode = 10, 'leaving'
-                        else:
-                            self.elevator.speed, self.elevator.mode = 0, 'boarding'
-                    elif self.elevator.mode == 'turning':
-                        self.elevator.speed, self.elevator.mode = 10, 'leaving'
-
-                    elif self.elevator.mode == 'idle':
-                        self.elevator.speed, self.elevator.mode = 10, 'leaving'
-        else:
-            self.elevator.speed, self.elevator.mode = 10, 'moving'
-
-    def first_come(self):
-        """
-        First come first serve algorithm
-        """
-        target_reached = False
-
         for floor in self.floors:
-            if self.elevator.y == floor.y and self.elevator.target == floor.name:
-                self.elevator.speed, self.elevator.mode = 0, 'boarding'
-                self.idle_events(floor)
-                if self.elevator.mode == 'idle':
-                    target_reached = True
-                    if self.elevator.second_target != '':
-                        self.elevator.target = self.elevator.second_target
-                        self.elevator.second_target = ''
-                        target_reached = False
+            if self.elevator.y == floor.y:
+                if floor.floor_type == 'end' and self.elevator.mode != 'leaving' and self.elevator.mode != 'idle':
+                    self.elevator.direction *= -1
+                    self.elevator.mode = 'turning'
 
-        if not target_reached:
-            for floor in self.floors:
-                if self.elevator.target == floor.name and self.elevator.y != floor.y:
-                    displacement = floor.y - self.elevator.y
-                    if displacement > 0:
-                        self.elevator.direction = 1
-                    elif displacement < 0:
-                        self.elevator.direction = -1
-                    self.elevator.speed = 10
+                if self.stop_check(floor):
+                    if self.elevator.mode == 'idle':
+                        self.floor_action(floor)
+                    else:
+                        self.elevator.mode, self.elevator.speed = 'idle', 0
+
+                elif not self.stop_check(floor):
+                    self.elevator.mode, self.elevator.speed = 'leaving', 10
+
+                if self.elevator.mode == 'leaving':
                     self.elevator.mode = 'moving'
 
-        elif target_reached and len(self.all_passengers) != 0:
-            for passenger in self.all_passengers:
-                self.elevator.target_passenger = passenger.name
-                self.elevator.target = passenger.source
-                self.elevator.second_target = passenger.destination
-                break
+    def first_come(self):
+        if self.elevator.passenger_target is None and len(self.all_passengers) != 0:
+            self.elevator.passenger_target = self.all_passengers[0]
+            if self.all_passengers[0] not in self.elevator.passengers:
+                self.elevator.target = self.all_passengers[0].source
+                self.elevator.second_target = self.all_passengers[0].destination
+            else:
+                self.elevator.target, self.elevator.second_target = self.all_passengers[0].destination, None
 
-        elif target_reached and len(self.all_passengers) == 0:
-            self.elevator.target = 'ground'
-            self.elevator.second_target = ''
+        elif self.elevator.target is None and len(self.all_passengers) == 0:
+            self.elevator.target = self.floors[0]
+            if self.elevator.y == self.floors[0].y:
+                self.elevator.target = None
+            self.elevator.second_target = None
+
+        for floor in self.floors:
+            if self.elevator.y == floor.y:
+                if floor.floor_type == 'end' and self.elevator.mode != 'leaving' and self.elevator.mode != 'idle':
+                    self.elevator.direction *= -1
+                    self.elevator.mode = 'turning'
+
+                if self.stop_check(floor):
+                    if self.elevator.mode == 'idle':
+                        self.floor_action(floor)
+                    else:
+                        self.elevator.mode, self.elevator.speed = 'idle', 0
+
+                elif not self.stop_check(floor):
+                    if self.elevator.target == floor:
+                        self.elevator.target, self.elevator.second_target = self.elevator.second_target, None
+                    self.elevator.mode, self.elevator.speed = 'leaving', 10
+
+                if self.elevator.mode == 'leaving':
+                    self.elevator.mode = 'moving'
+                    if self.elevator.target is not None:
+                        displacement = self.elevator.target.y - self.elevator.y
+                        if displacement > 0:
+                            self.elevator.direction = 1
+                        elif displacement < 0:
+                            self.elevator.direction = -1
+                    else:
+                        self.elevator.mode, self.elevator.speed = 'idle', 0
