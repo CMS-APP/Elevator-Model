b380ab2fbf0b456f0bb1559edd1d8641ccf5400b
Chris-App <Christopher.sharp@hotmail.co.uk>
2020-08-02 11:13:29 +0100

    Rename variables

--- elevator_sim/animations.py
+++ elevator_sim/animations.py
@@ -6,33 +6,36 @@ white = (255, 255, 255)
 black = (0, 0, 0)
 
 
-def animation_init(self):
+def animation_init(model):
     pygame.init()
     pygame.font.init()
     pygame.display.set_caption("Elevator Simulation")
-    self.elevator.image = pygame.image.load('lift.png')
-    self.elevator.image = pygame.transform.scale(self.elevator.image, (100, 100))
-    self.game_display = pygame.display.set_mode((DisplayWidth, DisplayHeight))
-    self.game_clock = pygame.time.Clock()
-    self.animation = True
-    self.simulation()
+    model.elevator.image = pygame.image.load('lift.png')
+    model.elevator.image = pygame.transform.scale(model.elevator.image, (100, 100))
+    model.game_display = pygame.display.set_mode((DisplayWidth, DisplayHeight))
+    model.game_clock = pygame.time.Clock()
+    model.animation = True
+    model.simulation()
 
 
-def message_display(self, text, font_size, text_x, text_y, colour):  # Function to simplify displaying messages
+def message_display(model, text, font_size, text_x, text_y, colour):  # Function to simplify displaying messages
     font = pygame.font.Font("freesansbold.ttf", font_size)  # uses font module to create a font
     display_text = font.render(str(text), True, colour)  # Creates the font with the text and the colour
-    self.game_display.blit(display_text, [text_x, text_y])  # Places to the x and y locations
+    model.game_display.blit(display_text, [text_x, text_y])  # Places to the x and y locations
 
 
-def animation_update(self):
-    self.game_display.fill(white)
-    for floor in self.floors:
-        pygame.draw.rect(self.game_display, black, (floor.x - 100, floor.y + 90, 100, 10))
-        message_display(self, floor.name, 20, floor.x - 75, floor.y + 70, black)
+def animation_update(model):
+    model.game_display.fill(white)
+    # Draw each floor
+    for floor in model.floors:
+        pygame.draw.rect(model.game_display, black, (floor.x - 100, floor.y + 90, 100, 10))
+        message_display(model, floor.name, 20, floor.x - 75, floor.y + 70, black)
         for passenger in range(len(floor.passengers)):
-            pygame.draw.rect(self.game_display, black, (floor.x - (passenger * 15) - 15, floor.y + 110, 10, 10))
-    self.game_display.blit(self.elevator.image, (self.elevator.x, self.elevator.y))
-    message_display(self, str(len(self.elevator.passengers)) + " / " + str(self.elevator.capacity), 20,
-                         self.elevator.x + 25, self.elevator.y - 20, black)
+            # Draw each passenger
+            pygame.draw.rect(model.game_display, black, (floor.x - (passenger * 15) - 15, floor.y + 110, 10, 10))
+    # Draw elevator and number of passengers inside
+    model.game_display.blit(model.elevator.image, (model.elevator.x, model.elevator.y))
+    message_display(model, str(len(model.elevator.passengers)) + " / " + str(model.elevator.capacity), 20,
+                    model.elevator.x + 25, model.elevator.y - 20, black)
     pygame.display.update()
-    self.game_clock.tick(self.frame_rate)
+    model.game_clock.tick(model.frame_rate)
