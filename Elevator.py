36eca8dd976aba4d518fc0e1f671d9b3a51fe066
Chris-App <Christopher.sharp@hotmail.co.uk>
2020-08-02 11:14:46 +0100

    Cleanup and comments and sorted readme file

--- Elevator.py
+++ Elevator.py
@@ -1,58 +1,56 @@
-from elevator_sim.objects import Floor, Elevator
+import pygame
+
 from elevator_sim.animations import animation_init, animation_update
 from elevator_sim.elevator_network import ElevatorNetwork
-import pygame
+from elevator_sim.objects import Floor, Elevator
 
 
 class ElevatorSim(ElevatorNetwork):
-    def __init__(self, floors, elevator, passengers, time_limit, frame_rate, mode):
-        super().__init__(floors, elevator, passengers, time_limit, frame_rate, mode)
+    def __init__(self, sim_floors, sim_elevator, sim_passengers, frame_rate, mode):
+        super().__init__(sim_floors, sim_elevator, sim_passengers, frame_rate, mode)
 
     def py_animation(self):
+        # If animation is enabled then initiate pygame
         animation_init(self)
 
     def simulation(self):
-        """
-        """
-        passenger_num = 0
         while self.time <= 1000 or self.animation:
-
             if self.animation:
                 for event in pygame.event.get():
+                    # Checks if the user quits the application
                     if event.type == pygame.QUIT:
                         quit()
 
-            passenger_num = self.new_passengers(passenger_num)
+            self.new_passengers()
+            # Spawns new passengers
 
-            if self.mode in self.modes:
-                if self.mode == 'linear':
-                    self.linear_simulation()
-                elif self.mode == 'first_come':
-                    self.first_come()
-                else:
-                    quit()
+            if self.mode == 'linear':
+                self.linear_simulation()
+            elif self.mode == 'first_come':
+                self.first_come()
             else:
                 quit()
 
             self.time += self.frame_rate
-            self.real_time = self.time/self.frame_rate
+            self.real_time = self.time / self.frame_rate
             if self.animation:
                 animation_update(self)
             self.elevator.update_position()
 
 
 floor_0 = Floor('ground', 'end', 600, 350, 0.03, [])
-floor_1 = Floor('1st', 'not_end', 600, 250, 0.01, [])
-floor_2 = Floor('2nd', 'not-end', 600, 150, 0.01, [])
-floor_3 = Floor('3rd', 'end', 600, 50, 0.01, [])
-floors_1 = [floor_0, floor_1, floor_2, floor_3]
+floor_1 = Floor('1st', 'not_end', 600, 250, 0.02, [])
+floor_2 = Floor('2nd', 'not-end', 600, 150, 0.02, [])
+floor_3 = Floor('3rd', 'end', 600, 50, 0.02, [])
+floors = [floor_0, floor_1, floor_2, floor_3]
 
-elevator_1 = Elevator(600, 350, -1, 10, 'leaving', 10, [])
-# elevator.mode = ['idle','leaving','moving','turning',]
+elevator = Elevator(600, 350, -1, 10, 'leaving', 10, [])
+# Elevator modes: 'idle', 'leaving', 'moving', 'turning'
 
-passengers_1 = []
+passengers = []
 
-Model_1 = ElevatorSim(floors_1, elevator_1, passengers_1, 100, 2, 'linear')
-# Model.mode = ['linear', 'first_come', 'point_system']
+# Example: ElevatorSim(floors, elevator, passengers, 2, 'linear').py_animation()
+# Example: ElevatorSim(floors, elevator, passengers, 2, 'linear').simulation()
 
-Model_1.simulation()
+ElevatorSim(floors, elevator, passengers, 2, 'first_come').py_animation()
+# Model modes: 'linear', 'first_come', 'point_system'
--- README.md
+++ README.md
@@ -2,27 +2,43 @@
 
 Small project which uses the pygame module to show an elevator moving up and down. The elevator picks up passengers and drops them off at other locations.
 
+
 ## Elevator Modes
 
 There are two modes avaliable in this program:
  * Linear - Elevator moves up and down the page
- * First Come first serve - Elevator picks up the first passenger which spawns and drops them off at their destination, then picks up the passenger which spawns next.
+ * First come first serve - Elevator picks up the first passenger which spawns and drops them off at their destination. Passengers are picked up in order of spawning.
+ 
  
 ## Getting Started
 
-When selecting which mode to use, change the mode parameter 'mode' to either 'linear' or 'first_come' in the main.py file.
+When selecting which mode to use, change the parameter 'mode' to either 'linear' or 'first_come' in the main.py file.
 
 ```
-ElevatorSim(floors, elevator, passengers, 100, 2, mode)
+ElevatorSim(floors, elevator, passengers, speed, mode)
 ```
 
 For example
 
 ```
-ElevatorSim(floors, elevator, passengers, 100, 2, 'linear')
-ElevatorSim(floors, elevator, passengers, 100, 2, 'first_come')
+ElevatorSim(floors, elevator, passengers, speed, 'linear')
+ElevatorSim(floors, elevator, passengers, speed, 'first_come')
+```
+
+
+## Simulation Modes 
+
+The program can either simulate the program with or without pygame. Using pygame will take longer, however the program visually shows the model. However, when not using the pygame animation the simulation runs much quicker until the time limit is reached - 1000 seconds by default.
+
+Using pygame animation:
+```
+ElevatorSim(floors, elevator, passengers, speed, mode).py_animation()
+```
+Otherwise simulate the model:
+```
+ElevatorSim(floors, elevator, passengers, speed, mode).simulation()
 ```
 
 ## Running Application
 
-After selecting the mode you would like to use, run the main.py file
+By default the model will have four floors, no passengers present. Also the elevator mode will be 'linear' and the simulation mode will be 'py_animation'. Once all the correct modes, floors and passengers are inputted, run the elevator.py file.
